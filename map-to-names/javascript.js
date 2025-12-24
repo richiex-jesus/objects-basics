a66bba03fca85d754e1f990e2ef85b8d709976a3
richiex-jesus <matiasrichard300@gmail.com>
2025-12-24 09:54:30 +0800

    completed map-to-names assignment

---
+++ map-to-names/javascript.js
@@ -0,0 +1,9 @@
+let user1 = {name: "Richard", age: 18};
+let user2 = {name: "Darryl", age: 21};
+let user3 = {name: "Isaac", age: 24};
+
+let arrUsers = [user1, user2, user3];
+
+let names = [user1.name, user2.name, user3.name];
+
+alert(names);
